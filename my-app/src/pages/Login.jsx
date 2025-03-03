52d7175b47fa0c0f0c43feb1c029d645b9e1a656
Jaymond Mach <jmach6@my.bcit.ca>
2025-03-03 12:07:04 -0800

    fixed login and register pages

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import GoogleButton from '../components/googlebutton';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import GoogleButton from "../components/googlebutton";
 
 function Login({ onLogin }) {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const formData = new FormData();
-      formData.append('username', email);
-      formData.append('password', password);
+      formData.append("username", email);
+      formData.append("password", password);
 
-      const response = await fetch('http://localhost:8000/token', {
-        method: 'POST',
-        body: formData
+      const response = await fetch("http://localhost:8000/token", {
+        method: "POST",
+        body: formData,
       });
       if (!response.ok) {
-        const errorData = await response.json()
+        const errorData = await response.json();
         throw new Error(errorData.detail);
       }
 
       const { access_token } = await response.json();
       onLogin(access_token);
-      navigate('/dashboard')
+      navigate("/dashboard");
     } catch (err) {
       setError(err.message);
     }
@@ -64,7 +64,10 @@ function Login({ onLogin }) {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form onSubmit={handleSubmit} className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
                 Email address
               </label>
               <div className="mt-2">
@@ -81,11 +84,17 @@ function Login({ onLogin }) {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
                   Password
                 </label>
                 <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
                     Forgot password?
                   </a>
                 </div>
@@ -105,54 +114,27 @@ function Login({ onLogin }) {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
               >
                 Sign in
               </button>
             </div>
-
           </form>
           <hr className="my-6 border-t border-gray-300" />
           <div className="mt-4">
             <GoogleButton />
           </div>
           <p className="mt-10 text-center text-sm/6 text-gray-500">
-            Not a member?{' '}
-            <Link to="/register" className="font-semibold text-indigo-600 hover:text-indigo-600">
+            Not a member?{" "}
+            <Link
+              to="/register"
+              className="font-semibold text-indigo-600 hover:text-indigo-600"
+            >
               Register
             </Link>
           </p>
-
         </div>
       </div>
-
-      {/* <div className="p-4 max-w-md mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Login</h1>
-      {error && <div className="text-red-500 mb-4">{error}</div>}
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label className="block mb-1">Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-        <div>
-          <label className="block mb-1">Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">
-          Login
-        </button>
-      </form>
-    </div> */}
     </>
   );
 }
--- my-app/src/pages/Register.jsx
+++ my-app/src/pages/Register.jsx
@@ -11,7 +11,7 @@ function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     if (password !== confirmPassword) {
       setError("Passwords do not match!");
       return;
@@ -22,16 +22,15 @@ function Register() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
-      }
-    );
+      });
 
-    if (!response.ok) {
+      if (!response.ok) {
         const errorData = await response.json();
-        throw new Error(errorData.detail || 'Registration failed');
+        throw new Error(errorData.detail || "Registration failed");
       }
 
       console.log("User registered successfully");
-      navigate('/')
+      navigate("/");
     } catch (err) {
       setError(err.message);
     }
@@ -50,7 +49,10 @@ function Register() {
 
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
-            <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
+            <label
+              htmlFor="name"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
               Full Name
             </label>
             <div className="mt-2">
@@ -67,7 +69,10 @@ function Register() {
           </div>
 
           <div>
-            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
               Email Address
             </label>
             <div className="mt-2">
@@ -84,7 +89,10 @@ function Register() {
           </div>
 
           <div>
-            <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+            <label
+              htmlFor="password"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
               Password
             </label>
             <div className="mt-2">
@@ -101,7 +109,10 @@ function Register() {
           </div>
 
           <div>
-            <label htmlFor="confirmPassword" className="block text-sm/6 font-medium text-gray-900">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
               Confirm Password
             </label>
             <div className="mt-2">
@@ -117,21 +128,22 @@ function Register() {
             </div>
           </div>
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Register
-            </button>
-          </div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
+          >
+            Register
+          </button>
         </form>
 
         <hr className="my-6 border-t border-gray-300" />
 
         <p className="mt-4 text-center text-sm/6 text-gray-500">
           Already have an account?{" "}
-          <Link to="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">
+          <Link
+            to="/login"
+            className="font-semibold text-indigo-600 hover:text-indigo-500"
+          >
             Sign in
           </Link>
         </p>
