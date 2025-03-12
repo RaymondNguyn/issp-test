07fca7ca96b2a464b3e02bccb5b78f1a46be67ef
Thenewgrid <H.K2348@outlook.com>
2025-03-12 16:27:53 -0700

    finalize algorithim and emails

--- backend/algorithim.py
+++ backend/algorithim.py
@@ -4,6 +4,14 @@ import time
 import threading
 import sys
 import os
+from email.message import EmailMessage
+import smtplib
+import ssl
+import certifi  # Use certifi for SSL certificates
+
+# PLEASE KEEP IN MIND:
+# When running my code you can't do keyboard interupts due to the use of threads.
+# To stop the program after execution simply kill the terminal.
 
 # 1 EXTRACT DATA
 file = sys.argv[1]
@@ -31,12 +39,21 @@ def clean_data():
 def get_list():
     store_list = []
     values = clean_data()
+    # add the names of the columns in the file into store_list index 0 to 9.
     for column in cols:
         store_list.append(column)
+    # add the data of the columns as lists in the file into store_list index 10 to 19.
     for column in cols:
         store_list.append(values[column].tolist())
-    
     return store_list
+
+# GET emial details
+email_sender = "hk580dev@gmail.com"
+email_password = "seorymxmfqqgczhe"
+email_reciever = "hkakooza1@my.bcit.ca"
+
+# Create SSL context for secure connection
+context_block = ssl.create_default_context(cafile=certifi.where())
 
 # Check the boundary
 def check_boundary(dataset,name):
@@ -51,14 +68,31 @@ def check_boundary(dataset,name):
         # Iterating over each value to see if its too high, too low or normal.
         for value in dataset:  
             # Waiting 30 minutes (1800 seconds) before it processes the value.
-            time.sleep(1)
+            time.sleep(10)
             if value > upper_limit:
                 # Sends a notification with the title of system alert and the message of which section sent the warning.
-                print(f"WARNING SECTION {name} IS ABNORMALLY HIGH: {value}")
+                body = f"WARNING SECTION {name} IS ABNORMALLY HIGH: {value}"
             elif value < lower_limit:
-                print(f"WARNING SECTION {name} IS ABNORMALLY LOW: {value}")
+                body = f"WARNING SECTION {name} IS ABNORMALLY LOW: {value}"
             else:
-                print(f"Normal for sec {name}, value = {value}")
+                # This value is not really necessary since it just lets the user know that their value is normal.
+                # It is just here for testing purposes.
+                body = f"Normal for sec {name}, value = {value}"
+            # Create the email
+            em = EmailMessage()
+            em['From'] = email_sender
+            em['To'] = email_reciever
+            em['Subject'] = name
+            em.set_content(body)
+            # Send email
+            # smpt.gmail.com (host) this defines where the sender email is coming from (in this case it has to be gmail).
+            # 465 is the port
+            # context is the SSL certificate
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context_block) as smtp:
+                # This is where the app logs in.
+                smtp.login(email_sender, email_password)
+                # This is the actual contents of the email.
+                smtp.sendmail(email_sender, email_reciever, em.as_string())
         # This prevents the loop from running constantly by setting run to False.
         run = False
         # MAYBE REMOVE THIS SINCE WE WANT THE LOOP TO RUN INDEFINITELY
@@ -69,9 +103,8 @@ def main():
     threads = []
     # get the length of the dataset
     length = len(cols)
-    # for each number in the length of columns, e.g 8 columns would be 0...7.
     for num in range(length):
-        # length of the cols and the 
+        # data_store will be the index of the lists of data from each column.
         data_store = (num + length)
         data_thread = threading.Thread(target=check_boundary,args=(get_list()[data_store],get_list()[num]))
         data_thread.start()
